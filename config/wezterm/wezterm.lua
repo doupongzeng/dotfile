b48c04d2e7461e08ca32e1e77a71a201a27a00f2
huangz16 <huangz16@xiaopeng.com>
2025-04-10 13:58:39 +0800

    feat: wezterm background image

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,28 +1,49 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
-local is_window_maximized = false
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function(cmd)
-    -- 获取主屏幕的信息
-    local screen = wezterm.gui.screens().main
-    -- 定义窗口大小（例如屏幕宽高的70%）
-    local ratio = 0.7
-    local width = screen.width * ratio
-    local height = screen.height * ratio
+local static_is_window_maximized = false
+local static_current_image_index = 0
 
-    -- 启动一个新窗口
-    local tab, pane, window = mux.spawn_window(cmd or {})
+local home_dir = wezterm.home_dir
 
-    -- 设置窗口大小
-    window:gui_window():set_inner_size(width, height)
+wezterm.on("gui-startup", function(cmd)
+  -- 获取主屏幕的信息
+  local screen = wezterm.gui.screens().main
+  -- 定义窗口大小（例如屏幕宽高的70%）
+  local ratio = 0.7
+  local width = screen.width * ratio
+  local height = screen.height * ratio
 
-    -- 计算窗口的中心位置
-    local x = (screen.width - width) / 2
-    local y = (screen.height - height) / 2
+  -- 启动一个新窗口
+  local tab, pane, window = mux.spawn_window(cmd or {})
 
-    -- 将窗口移动到屏幕中央
-    window:gui_window():set_position(x, y)
+  -- 设置窗口大小
+  window:gui_window():set_inner_size(width, height)
+
+  -- 计算窗口的中心位置
+  local x = (screen.width - width) / 2
+  local y = (screen.height - height) / 2
+
+  -- 将窗口移动到屏幕中央
+  window:gui_window():set_position(x, y)
+end)
+
+wezterm.on("toggle-image", function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  local image_paths = {
+    home_dir .. "/dotfile/images/04190_clouds_1680x1050.jpg",
+    home_dir .. "/dotfile/images/04180_grandtetonsunset_1680x1050.jpg",
+    "",
+  }
+
+  static_current_image_index = static_current_image_index + 1
+  if static_current_image_index > #image_paths then
+    static_current_image_index = 1
+  end
+
+  overrides.window_background_image = image_paths[static_current_image_index]
+  window:set_config_overrides(overrides)
 end)
 
 return {
@@ -38,10 +59,14 @@ return {
   initial_cols = 120,                        -- 默认列数
   initial_rows = 30,                         -- 默认行数
   window_close_confirmation = "AlwaysPrompt", -- 关闭窗口时提示
+  -- window_background_image = home_dir .. "/dotfile/images/04180_grandtetonsunset_1680x1050.jpg",
+  -- window_background_image = home_dir .. "/dotfile/images/04190_clouds_1680x1050.jpg",
 
   -- 字体设置
   font = wezterm.font("Maple Mono Normal NL NF CN", { weight = "Regular", style = "Normal" }),
   font_size = 13.0,
+
+  -- window_background_gradient
 
   -- 颜色方案
   colors = {
@@ -116,12 +141,12 @@ return {
       key = "F11",
       mods = "",
       action = wezterm.action_callback(function(window, _pane)
-        if is_window_maximized then
+        if static_is_window_maximized then
           window:restore()
-          is_window_maximized = false
+          static_is_window_maximized = false
         else
           window:maximize()
-          is_window_maximized = true
+          static_is_window_maximized = true
         end
       end),
     },
@@ -130,6 +155,11 @@ return {
       key = "v",
       mods = "CTRL",
       action = act.PasteFrom("Clipboard"), -- 从剪贴板粘贴
+    },
+    {
+      key = ",",
+      mods = "SUPER",
+      action = act.EmitEvent("toggle-image"), -- 从剪贴板粘贴
     },
   },
 
